d6c77b01e4c771974ab38cc31c78d8098d937c2e
Igor Dumchykov <Igor_Dumchykov@neimanmarcus.com>
2022-08-09 17:56:09 +0300

    Add log levels

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const configService = app.get<ConfigService>(ConfigService);
+  const logLevels = configService.get('app_log_levels').split(',');
+  console.log(logLevels)
+
   await app.listen(3000);
 }
 bootstrap();
